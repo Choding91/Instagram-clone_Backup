1ff98f839a225ef7cb48d5be8ba703be78649d2f
LeeHyunji <hyunji015@gmail.com>
2022-09-30 16:04:33 +0900

    feature/1 - 게시글, 댓글 Model 생성
    모델 >
     Feed
        - 작성자
        - 내용
        - 생성일자
        - 수정일자
     FeedComment
        - feed_id
        - 작성자
        - 내용
        - 생성일자
    
    뷰 > view.py에서 역활 분담

--- sns/views.py
+++ sns/views.py
@@ -1,6 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# 메인
 def main(request):
-    
     return render(request, 'sns/index.html')
+
+# 전체 피드 - 현지
+def feed(request):
+    return render(request, 'sns/index.html')
+
+# 피드 작성 - 현지
+def feed_create(request):
+    return render(request, 'sns/index.html')
+
+# 피드 업데이트 - 승주님
+def feed_update(request):
+    return render(request, 'sns/index.html')
+
+# 피드 삭제 - 승주님
+def feed_delete(request):
+    return render(request, 'sns/index.html')
+
+# 댓글 - 상훈
