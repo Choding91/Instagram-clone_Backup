2d8d744c88b008cab44e3387d4a2a7d2c372546a
LeeHyunji <hyunji015@gmail.com>
2022-10-05 19:54:25 +0900

    !HOTFIX: 로그인 미인증으로인한 버그 해결
    로그인 auth에 관한 모든 method 사용이 불가했던 버그가 있었는데
    user/view.py에서 signin시 auth.login을 하지 않아서 생기는 문제로 판명.

--- user/views.py
+++ user/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from .models import User
 from django.contrib.auth.hashers import make_password, check_password
 from rest_framework.response import Response
+from django.contrib import auth
 
 
 class Signin(APIView):
@@ -14,6 +15,7 @@ class Signin(APIView):
         email = request.data.get('email', None)
         password = request.data.get('password', None)
 
+        
         user = User.objects.filter(email=email).first()
 
         if email == '':
@@ -25,6 +27,8 @@ class Signin(APIView):
         elif check_password(password, user.password) is False:
             return Response(status=500, data=dict(message='비밀번호가 잘못되었습니다.'))
         else:
+            me = auth.authenticate(request, username=user.username, password=password)
+            auth.login(request, me)
             request.session['loginCheck'] = True
             request.session['email'] = user.email
             return Response(status=200, data=dict(message='로그인에 성공했습니다.'))
